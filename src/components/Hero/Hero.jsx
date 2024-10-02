b285c1ad094e8eef500448cadcbeffa4f5a6025e
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-10-01 19:52:43 -0700

    work

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useState, useEffect } from 'react';
 import "./Hero.css";
 
 const DynamicHeroText = () => {
-  const [jobTitle, setJobTitle] = useState('developer');
+  const [jobTitle, setJobTitle] = useState('Full-Stack Web Developer');
 
   useEffect(() => {
-    const titles = ['developer', 'programmer', 'web developer', 'software engineer'];
+    const titles = ['Full-Stack Web Developer', 'Programmer', 'Software Engineer'];
 
     const intervalId = setInterval(() => {
       setJobTitle(prevTitle => {
@@ -13,17 +14,18 @@ const DynamicHeroText = () => {
         const nextIndex = (currentIndex + 1) % titles.length;
         return titles[nextIndex];
       });
-    }, 2000);
+    }, 1500);
 
     return () => clearInterval(intervalId);
   }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-red-500 ">
+    <div className="flex items-center justify-center h-screen bg-red-200">
+
       <div className="text-center">
-        <h1 className="text-4xl">Hello, my name is Caleb</h1>
-        <p className="py-6 text-2xl">
-          I&apos;m a <span className="font-bold text-primary">{jobTitle}</span>
+
+        <h1 className="text-4xl">Hello, I'm Caleb! I'm a </h1>
+        <p className="py-6 text-2xl"><span className="font-bold text-primary">{jobTitle}</span>
         </p>
       </div>
     </div>
