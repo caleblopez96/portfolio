d2bfde5ba6627c5234852b5e8c6f7e168f13d9da
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-10-01 19:06:25 -0700

    idk?

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
-import "./Hero.css"
+import "./Hero.css";
 
 const DynamicHeroText = () => {
   const [jobTitle, setJobTitle] = useState('developer');
 
   useEffect(() => {
-    const titles = ['developer', 'programmer', 'web developer', 'software engineer']; // Move inside useEffect
+    const titles = ['developer', 'programmer', 'web developer', 'software engineer'];
 
     const intervalId = setInterval(() => {
       setJobTitle(prevTitle => {
@@ -15,14 +15,14 @@ const DynamicHeroText = () => {
       });
     }, 2000);
 
-    return () => clearInterval(intervalId); // Clean up interval on unmount
-  }, []); // No need for 'titles' in the dependency array
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
-    <div id='container' className="">
-      <div className="hero-content">
+    <div id='container' className="flex items-center justify-center h-screen">
+      <div className="text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold font-main flex justify-center">Hello, my name is Caleb</h1>
+          <h1 className="text-3xl font-main">Hello, my name is Caleb</h1>
           <p className="py-6 text-2xl">
             I&apos;m a <span className="font-bold text-primary">{jobTitle}</span>
           </p>
