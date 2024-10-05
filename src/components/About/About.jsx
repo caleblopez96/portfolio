504ded65b3a3e7f6118c2779254e6e6cb5f39aa5
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-10-05 16:28:07 -0700

    changes to about and hero

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,9 @@
 const About = () => {
   return (
 
-    <div className="h-screen">
-      <h1 className="">About Me</h1>
-      <p>test test test test test</p>
+    <div className="h-screen px-4 py-2 font-mono">
+      <h1 className="text-4xl font-bold mb-4">About Me</h1>
+      <p>Full-time student currently pursuing my Bachelor of Science in Graphic Information Technology with a focus in full-stack web development from Arizona State University. I am passionate about solving problems with code, and I thrive on the challenges that web development offers. Capable of handling front-end and back-end tasks, I am eagerly seeking full-stack internship opportunities for the summer of 2025 to showcase my knowledge and learn from industry professionals. My dream is to turn this passion in to a career.</p>
     </div>
   )
 }
