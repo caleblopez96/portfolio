cc5113941772dadee61420ef90ebbd4bbf96f1e3
Caleb <87406555+caleblopez96@users.noreply.github.com>
2024-10-05 17:40:15 -0700

    small edits to type and styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,7 @@ import ToolGrid from './ToolGrid'; // Adjust the path according to your folder s
 const About = () => {
   return (
     <>
-      <div className="min-h-screen px-4 py-10 font-mono">
+      <div className="min-h-screen px-3 py-10 font-mono">
         <h1 className="text-3xl font-bold mb-4 text-red-700">About Me</h1>
         <p className=" text-base leading-relaxed">
           Full-time student currently pursuing my Bachelor of Science in Graphic Information Technology with a focus in full-stack web development from Arizona State University. I am passionate about solving problems with code, and I thrive on the challenges that web development offers. Capable of handling front-end and back-end tasks, I am eagerly seeking full-stack internship opportunities for the summer of 2025 to showcase my knowledge and learn from industry professionals. My dream is to turn this passion into a career.
