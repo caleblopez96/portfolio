ff429ebfe9c68fb14fe3c20fb04a970d5f771082
caleblopez96 <caleblopez96@gmail.com>
2024-10-08 20:23:51 -0700

    see last commit msg

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <>
       <div className="min-h-screen px-3 py-10 font-mono border-b-4 border-b-red-700">
         <h1 id="About" className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-mono font-bold text-red-700">About Me</h1>
-        <h2 className="text-xl sm:text-3xl md:text-4xl lg:text-5xl mt-2">Turning my passion into a career...</h2>
+        <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl mt-2">Turning my passion into a career...</h2>
         <p className="text-lg sm:text-xl md:text-2xl lg:text-2xl text-balance py-3">
           Thanks to my GI-BILL, I’m diving deep into full-stack web development at Arizona State University. Web development constantly challenges me, and I love working across both the front-end and back-end. I’m on the lookout for full-stack internships in summer 2025 where I can put my skills to the test, learn, and contribute to some exciting projects.
         </p>
