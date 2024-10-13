c79d6129ef81b2e234b3c118f16ac476ac83e822
caleblopez96 <caleblopez96@gmail.com>
2024-10-13 10:29:09 -0700

    moved css into a global folder and repathed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
-import '../css/App.css'
+// import '../css/app.css'
+import '../src/css/app.css'
 import HomePage from './pages/Homepage.jsx'
 import { ThemeProvider } from './ThemeContext'
 
