9f09b47c917602aa806e09e5cc3ccc7df8578d36
caleblopez96 <caleblopez96@gmail.com>
2025-01-30 19:58:00 -0700

    removed unecessary comments

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,7 +7,7 @@ import Contact from "../components/Contact/Contact"
 const HomePage = () => {
   return (
     <div>
-      <NavBar /> {/* Use NavBar as a component */}
+      <NavBar />
       <Hero />
       <About />
       <Contact />
